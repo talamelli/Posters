f1c0a049a1e1dcdf202aacbf7741a7f38c7fe107
talamelli <lorenzo.talamelli18@gmail.com>
2015-11-08 20:48:15 +0100

    Some Changes
    Little....

--- Talamelli.Lorenzo.5L.Posters/Talamelli.Lorenzo.5L.Posters/Models/Notizia.cs
+++ Talamelli.Lorenzo.5L.Posters/Talamelli.Lorenzo.5L.Posters/Models/Notizia.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Web;
-using System.Xml;
 using System.Xml.Linq;
-using Terradue.ServiceModel.Syndication;
 
 namespace Talamelli.Lorenzo._5L.Posters
 {
@@ -32,16 +27,18 @@ namespace Talamelli.Lorenzo._5L.Posters
             foreach (Fonte Url in GetNotizie.LUrl)
             {
                 XDocument doc = XDocument.Parse(GetNotizie.DownloadString(Url.URL));
+                try {
+                    giornale.AddRange((from descendant in doc.Descendants("item")
+                                       select new Notizia()
+                                       {
 
-               giornale.AddRange((from descendant in doc.Descendants("item")
-                    select new Notizia()
-                    {
-                        
-                        Descrizione = getDescr(descendant.Element("description").Value) ,
-                        Titolo = Encoder(descendant.Element("title").Value),
-                        SitoProvenienza = Url.Titolo,
-                      
-                    }).ToList());
+                                           Descrizione = getDescr(descendant.Element("description").Value),
+                                           Titolo = Encoder(descendant.Element("title").Value),
+                                           SitoProvenienza = Url.Titolo,
+
+                                       }).ToList());
+                }
+                catch (Exception err) { giornale.Add(new Notizia { Titolo = "Errore nell'elaborazione, Prova a ricaricare la pagina", Descrizione = err.Message, SitoProvenienza = "Sorry... " }); }
 
             }
             return giornale;
